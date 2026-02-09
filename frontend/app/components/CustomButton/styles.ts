032f1d1ec4c2105de4523c5634ee53fd04326f55
Josep Guiu Sillés <jguius2021@cepnet.net>
2026-02-09 18:46:05 +0100

    PUSH Total Pepe

---
+++ frontend/app/components/CustomButton/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    backgroundColor: '#5b55c0',
+    paddingVertical: 14,
+    borderRadius: 10,
+    alignItems: 'center',
+    width: "50%",
+    marginTop: "2%",
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  disabled: {
+    opacity: 0.5,
+  },
+});
