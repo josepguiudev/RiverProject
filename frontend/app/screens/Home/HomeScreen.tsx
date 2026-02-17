b7ceac3104156709c00e6c0089c036d6ee84502c
Josep Guiu Sillés <jguius2021@cepnet.net>
2026-02-17 20:16:24 +0100

    Arreglo de fallo que no deja iniciar

---
+++ frontend/app/screens/Home/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import globalStyles from "@/assets/globalStyles/globalStyles";
+
+export default function HomeScreen({ navigation }: any) {
+    return (
+        <View style={globalStyles.padre}>
+
+        </View>
+    );
+}
---
+++ frontend/app/screens/Home/styles.ts
@@ -0,0 +1,5 @@
+import { StyleSheet, Platform } from 'react-native';
+
+export default StyleSheet.create({
+
+});
