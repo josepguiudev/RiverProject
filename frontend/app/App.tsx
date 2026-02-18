63cb8caa87bb023beb0858f842451772d9a39488
Josep Guiu Sillés <jguius2021@cepnet.net>
2026-02-18 19:25:48 +0100

    Elimino cosas con errores que no hacen nada util

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,7 +1,6 @@
 // app/App.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { AppNavigator } from './navigation/AppNavigator';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 /*import HomeScreen from './screens/Home/HomeScreen';*/
