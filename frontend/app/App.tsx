c04c68ef70bb8d653bd3c3301f409fa088078288
IDunklet <dunkletodeus@gmail.com>
2026-02-19 20:21:58 +0100

    cambios sin errores

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -4,14 +4,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // 1. Importaciones de tus pantallas
-import SignIn from './components/auth/signin/signin'; 
+
 import RegisterScreen from './screens/Auth/RegisterScreen';
 import SurveyCreatorScreen from './screens/SurveyCreatorScreen';
 import HomeScreen from './screens/Home/HomeScreen';
 
+import LoginScreen from './screens/Auth/LoginScreen';
+
 // 2. INTERRUPTOR DIRECTO
 // Cambia a true para ver Storybook, false para la App normal
-const SHOW_STORYBOOK = true;
+const SHOW_STORYBOOK = false;
 
 const Stack = createNativeStackNavigator();
 
@@ -34,34 +36,4 @@ function App() {
     </NavigationContainer>
   );
 }
-
-/*
-<Stack.Screen name="Home" component={HomeScreen} />
-<AppNavigator />
-*/
-
-/*
-//nuevo app para pruebas moha
-import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import SimpleFormScreen from './screens/SurveyCreatorScreen';
-import { View, StatusBar, StyleSheet } from 'react-native';
-import SimpleFormScreen from './screens/SimpleFormScreen';
-
-const App: React.FC = () => {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor="#f5f5f5" />
-      <SimpleFormScreen />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-});
-
 export default App;
